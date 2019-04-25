c73c49eba2a85edfb571b928a0b83d5b030e7e50
Mamoon212 <aftermath202@gmail.com>
2019-04-25 20:39:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/calculator/CalculatorViewModel.kt
@@ -0,0 +1,91 @@
+package com.example.calculator
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Transformations
+import android.arch.lifecycle.ViewModel
+
+class CalculatorViewModel: ViewModel() {
+    private var operand1: Double?= null
+    private var pendingOperation= "="
+    private val result= MutableLiveData<Double>()
+    val stringResult: LiveData<String>
+    get() = Transformations.map(result) {it.toString()}
+
+    private val newNumber= MutableLiveData<String>()
+    val stringNewNumber: LiveData<String>
+    get() = newNumber
+
+    private val operation= MutableLiveData<String>()
+    val stringOperation: LiveData<String>
+    get() = operation
+
+    fun digitPressed(caption: String){
+        if(newNumber.value != null){
+            newNumber.value= newNumber.value+ caption
+        }else{
+            newNumber.value= caption
+        }
+    }
+
+    fun operandPressed(op: String){
+        try{
+            val value= newNumber.value?.toDouble()
+            if (value != null) {
+                performOperation(value, op)
+            }
+        }
+        catch (e: NumberFormatException){
+            newNumber.value= ""
+        }
+        pendingOperation= op
+        operation.value= pendingOperation
+    }
+
+    fun negPressed(){
+        val value= newNumber.value.toString()
+        if(value.isEmpty()){
+            newNumber.value= "-"
+        }else{
+            try{
+                var doubleValue= value.toDouble()
+                doubleValue *= -1
+                newNumber.value= doubleValue.toString()
+            } catch (e: NumberFormatException){
+                newNumber.value= ""
+            }
+        }
+    }
+
+    fun clrPressed(){
+        newNumber.value= ""
+        result.value= 0.0
+        operation.value= "="
+        operand1= null
+        pendingOperation= "="
+    }
+
+    private fun performOperation(value: Double, operation:String){
+        if(operand1==null){
+            operand1= value
+        }else{
+            if(pendingOperation=="="){
+                pendingOperation = operation
+            }
+
+            when(pendingOperation){
+                "=" -> operand1 = value
+                "/" -> operand1 = if(value==0.0){
+                    Double.NaN
+                }else{
+                    operand1!! / value
+                }
+                "*" -> operand1= operand1!! * value
+                "-" -> operand1= operand1!! - value
+                "+" -> operand1= operand1!! + value
+            }
+        }
+        result.value= operand1
+        newNumber.value= ""
+    }
+}
